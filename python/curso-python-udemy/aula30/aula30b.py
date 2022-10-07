30d49bc8680cf67ec1ce78189913a2fa4eb6cc68
Bruno Henrique da Silva Homem Ferreira <brunohhomem@gmail.com>
2022-10-07 18:36:53 -0300

    Dicionarios em python

---
+++ python/curso-python-udemy/aula30/aula30b.py
@@ -0,0 +1,24 @@
+# Dicionarios dentro de dicionarios
+
+clientes = {
+    'cliente1': {
+        'nome': 'Bruno',
+        'sobrenome': 'Homem',
+    },
+    'cliente2': {
+        'nome': 'Butters',
+        'sobrenome': 'Homem',
+    },
+    'cliente3': {
+        'nome': 'Vitória',
+        'sobrenome': 'Soares',
+    },
+}
+
+# Iteracao de dicionario dentro de dicionario
+for clientes_k, clientes_v in clientes.items():
+    print(f'Exibindo {clientes_k}')
+    for dados_k, dados_v in clientes_v.items():
+        print(f'\t{dados_k} = {dados_v}')
+
+
---
+++ python/curso-python-udemy/aula30/aula30c.py
@@ -0,0 +1,7 @@
+d1 = {1: 'a', 2: 'b', 3: 'c'}
+v = d1
+
+v[1] = 'Bruno'
+
+print(d1)
+print(v)
