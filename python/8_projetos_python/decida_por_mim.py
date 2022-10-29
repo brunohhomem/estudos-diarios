c97a9ac6defa96cbe44c09e2289f13350c0e6ef5
Bruno Henrique da Silva Homem Ferreira <brunohhomem@gmail.com>
2022-10-29 08:49:13 -0300

    projetos 5 e 6

---
+++ python/8_projetos_python/decida_por_mim.py
@@ -0,0 +1,33 @@
+# - Projeto 5: Decida por mim;
+import random
+import PySimpleGUI as sg
+
+
+class DecidaPorMim:
+    def __init__(self):
+        self.respostas = [
+            'Com certeza, você deve fazer isso!',
+            'Não sei. Você quem sabe.',
+            'Não faça isso.',
+            'Acho que está na hora certa!'
+        ]
+
+    def Iniciar(self):
+        # Layout
+        layout = [
+            [sg.Text('Faça sua pergunta:')],
+            [sg.Input()],
+            [sg.Output(size=(45, 10))],
+            [sg.Button('Decida por mim')]
+        ]
+        # Criar Janela
+        self.janela = sg.Window('Decida por mim', layout=layout)
+        while True:
+            # Ler os valores
+            self.eventos, self.valores = self.janela.Read()
+            # Fazer algo com os valores
+            if self.eventos == 'Decida por mim':
+                print(random.choice(self.respostas))
+
+decida = DecidaPorMim()
+decida.Iniciar()
