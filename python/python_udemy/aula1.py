3ba166711eabbc264d604c1db36c66d4e42affdb
Bruno Henrique da Silva Homem Ferreira <brunohhomem@gmail.com>
2022-11-10 18:04:28 -0300

    58/365

---
+++ python/python_udemy/aula1.py
@@ -0,0 +1,9 @@
+# Comentário
+
+"""
+Isso não é um comentário, é um 'DocString'
+Mas é usado como comentários multi-linha
+"""
+
+print('Olá.','Tudo bem?') # Exibe valores na tela, pode passar mais de um valor, usando virgula como separador.
+print(1, 2, 3, 4, 5, sep='_')
