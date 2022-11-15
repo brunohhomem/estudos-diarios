4f045446c0452a7af5c10260ae6a5359a9e97a0f
Bruno Henrique da Silva Homem Ferreira <brunohhomem@gmail.com>
2022-11-15 18:01:12 -0300

    63/365

---
+++ python/python_udemy/secao_iniciando/aula6.py
@@ -0,0 +1,16 @@
+c1 = False
+c2 = False
+c3 = False
+c4 = True
+
+if c1:
+  print('Condição 1')
+elif c2:
+  print('Condição 2')
+elif c3:
+  print('Condição 3')
+elif c4:
+  c1 = True
+  print('Condição 4')
+else:
+  print('Nenhuma condição verdadeira')
