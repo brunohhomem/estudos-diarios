cc5493c408b54326ca7f1bf833564e4abd8723ad
Bruno H. Homem <brunohhomem@gmail.com>
2022-12-13 08:11:56 -0300

    Estudos diários 02

--- python/python_udemy/secao_iniciando/aula15.py
+++ python/python_udemy/secao_iniciando/aula15.py
@@ -1,4 +1,14 @@
-texto = 'Python'
+# texto = 'Python'
 
-for letra in texto:
-    print(letra)
+# for letra in texto:
+#     print(letra)
+
+"""
+For + range
+range -> range(start, stop, step)
+"""
+
+numeros = range(0, 101, 10)
+
+for numero in numeros:
+    print(numero)
